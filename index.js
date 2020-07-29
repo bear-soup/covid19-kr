3c6245d449ae6b44fe6c43a12a5931a19e4bc72b
존령 <sapa1313@naver.com>
2020-07-29 17:12:34 +0900

    feat: 날짜 사이의 확진자, 완치자, 사망자 반환하는 함수 추가

--- index.js
+++ index.js
@@ -10,6 +10,8 @@
 
 const axios = require('axios')
 const moment = require('moment')
+const xml2js = require('xml2js')
+const request = require('request')
 
 module.exports = {
     Covid19KR: function(options) {
@@ -30,29 +32,29 @@ class Covid19 {
         this.numOfRows = 10
     }
 
-    async getCovidKRStatus(rows = 10, pageNo = 1, startDate = moment().subtract(1, 'month').format('yyyyMMDD'),
-        endDate = moment().format('yyyyMMDD')) {
+    // async getCovidKRStatus(rows = 10, pageNo = 1, startDate = moment().subtract(1, 'month').format('yyyyMMDD'),
+    //     endDate = moment().format('yyyyMMDD')) {
 
-        return this.requestCovid(
-            Covid19.RequestInfo.Path.getCovid19InfStateJson,
-            rows,
-            pageNo,
-            startDate,
-            endDate
-        )
-    }
+    //     return this.requestCovid(
+    //         Covid19.RequestInfo.Path.getCovid19InfStateJson,
+    //         rows,
+    //         pageNo,
+    //         startDate,
+    //         endDate
+    //     )
+    // }
 
     async getCovidKRByState(rows = 10, pageNo = 1, startDate = moment().subtract(1, 'month').format('yyyyMMDD'),
         endDate = moment().format('yyyyMMDD')) {
-            
+                
         return this.requestCovid(Covid19.RequestInfo.Path.getCovid19SidoInfStateJson,
             rows,
             pageNo,
             startDate,
             endDate
-        )
+        )   
     }
-
+    
     async requestCovid(requestBaseURL, rows, pageNo, startDate, endDate) {
         const requestURL = requestBaseURL +
             `?serviceKey=${this.apiKey}&numOfRows=${rows}` + 
@@ -74,5 +76,189 @@ class Covid19 {
         // const itemList = responseBody.items.item
 
         return responseBody
+    }
+
+     
+    async getCovidStatisticsBetweenClear(begin, end)
+    {
+        var pageNo = 1
+        var rows = 10
+        
+        const requestURL = Covid19.RequestInfo.Path.getCovid19SidoInfStateJson +
+            `?serviceKey=${this.apiKey}&numOfRows=${rows}` + 
+            `&pageNo=${pageNo}&startCreateDt=${begin}&endCreateDt=${end}`
+
+        var parser = new xml2js.Parser()
+        var isolClearCnt = 0
+        request(url, function(error, response, body) {     
+            parser.parseString(body, function(err,result){
+                var obj = JSON.stringify(result)
+                
+                var jsonArray = JSON.parse(obj).response.body[0].items[0].item
+                var days = end-begin
+                for(var i = 18; i < jsonArray.length; i = i + (days * 19))
+                {
+                    var array = JSON.stringify(jsonArray[i])
+                    if(i == 18)
+                        isolClearCnt = parseInt(JSON.parse(array).isolClearCnt[0])
+                    else
+                        isolClearCnt = isolClearCnt - parseInt(JSON.parse(array).isolClearCnt[0])
+                }
+            })     
+        })
+        return isolClearCnt
+    }
+
+    async getCovidStatisticsBetweenSick(begin, end)
+    {
+        var pageNo = 1
+        var rows = 10
+
+        const requestURL = Covid19.RequestInfo.Path.getCovid19SidoInfStateJson +
+            `?serviceKey=${this.apiKey}&numOfRows=${rows}` + 
+            `&pageNo=${pageNo}&startCreateDt=${begin}&endCreateDt=${end}`
+
+        var parser = new xml2js.Parser()
+        var defCnt = 0
+        request(url, function(error, response, body) {     
+            parser.parseString(body, function(err,result){
+                var obj = JSON.stringify(result)
+                
+                var jsonArray = JSON.parse(obj).response.body[0].items[0].item
+                var days = end-begin
+                for(var i = 18; i < jsonArray.length; i = i + (days * 19))
+                {
+                    var array = JSON.stringify(jsonArray[i])
+                    if(i == 18)
+                    defCnt = parseInt(JSON.parse(array).defCnt[0])
+                    else
+                    defCnt = defCnt - parseInt(JSON.parse(array).defCnt[0])
+                }
+            })     
+        })
+        return defCnt
+    }
+
+    async getCovidStatisticsBetweenDeath(begin, end)
+    {
+        var pageNo = 1
+        var rows = 10
+
+        const requestURL = Covid19.RequestInfo.Path.getCovid19SidoInfStateJson +
+            `?serviceKey=${this.apiKey}&numOfRows=${rows}` + 
+            `&pageNo=${pageNo}&startCreateDt=${begin}&endCreateDt=${end}`
+
+        var parser = new xml2js.Parser()
+        var deathCnt = 0
+        request(url, function(error, response, body) {     
+            parser.parseString(body, function(err,result){
+                var obj = JSON.stringify(result)
+                
+                var jsonArray = JSON.parse(obj).response.body[0].items[0].item
+                var days = end-begin
+                for(var i = 18; i < jsonArray.length; i = i + (days * 19))
+                {
+                    var array = JSON.stringify(jsonArray[i])
+                    if(i == 18)
+                    deathCnt = parseInt(JSON.parse(array).deathCnt[0])
+                    else
+                    deathCnt = deathCnt - parseInt(JSON.parse(array).deathCnt[0])
+                }
+            })     
+        })
+        return deathCnt
+    }
+
+    async getCovidStatisticsDayClear(day)
+    {
+        var pageNo = 1
+        var rows = 10
+        var begin = day - 1
+
+        const requestURL = Covid19.RequestInfo.Path.getCovid19SidoInfStateJson +
+            `?serviceKey=${this.apiKey}&numOfRows=${rows}` + 
+            `&pageNo=${pageNo}&startCreateDt=${begin}&endCreateDt=${day}`
+
+        var parser = new xml2js.Parser()
+        var deathCnt = 0
+        request(url, function(error, response, body) {     
+            parser.parseString(body, function(err,result){
+                var obj = JSON.stringify(result)
+                
+                var jsonArray = JSON.parse(obj).response.body[0].items[0].item
+                var days = end-begin
+                for(var i = 18; i < jsonArray.length; i = i + (days * 19))
+                {
+                    var array = JSON.stringify(jsonArray[i])
+                    if(i == 18)
+                    deathCnt = parseInt(JSON.parse(array).deathCnt[0])
+                    else
+                    deathCnt = deathCnt - parseInt(JSON.parse(array).deathCnt[0])
+                }
+            })     
+        })
+        return deathCnt
+    }
+
+    async getCovidStatisticsDaySick(day)
+    {
+        var pageNo = 1
+        var rows = 10
+        var begin = day - 1
+
+        const requestURL = Covid19.RequestInfo.Path.getCovid19SidoInfStateJson +
+            `?serviceKey=${this.apiKey}&numOfRows=${rows}` + 
+            `&pageNo=${pageNo}&startCreateDt=${begin}&endCreateDt=${end}`
+
+        var parser = new xml2js.Parser()
+        var deathCnt = 0
+        request(url, function(error, response, body) {     
+            parser.parseString(body, function(err,result){
+                var obj = JSON.stringify(result)
+                
+                var jsonArray = JSON.parse(obj).response.body[0].items[0].item
+                var days = end-begin
+                for(var i = 18; i < jsonArray.length; i = i + (days * 19))
+                {
+                    var array = JSON.stringify(jsonArray[i])
+                    if(i == 18)
+                    deathCnt = parseInt(JSON.parse(array).deathCnt[0])
+                    else
+                    deathCnt = deathCnt - parseInt(JSON.parse(array).deathCnt[0])
+                }
+            })     
+        })
+        return deathCnt
+    }
+
+    async getCovidStatisticsDayDeath(day)
+    {
+        var pageNo = 1
+        var rows = 10
+        var begin = day - 1
+
+        const requestURL = Covid19.RequestInfo.Path.getCovid19SidoInfStateJson +
+            `?serviceKey=${this.apiKey}&numOfRows=${rows}` + 
+            `&pageNo=${pageNo}&startCreateDt=${begin}&endCreateDt=${end}`
+
+        var parser = new xml2js.Parser()
+        var deathCnt = 0
+        request(url, function(error, response, body) {     
+            parser.parseString(body, function(err,result){
+                var obj = JSON.stringify(result)
+                
+                var jsonArray = JSON.parse(obj).response.body[0].items[0].item
+                var days = end-begin
+                for(var i = 18; i < jsonArray.length; i = i + (days * 19))
+                {
+                    var array = JSON.stringify(jsonArray[i])
+                    if(i == 18)
+                    deathCnt = parseInt(JSON.parse(array).deathCnt[0])
+                    else
+                    deathCnt = deathCnt - parseInt(JSON.parse(array).deathCnt[0])
+                }
+            })     
+        })
+        return deathCnt
     }
 }
